051cb6685e98460ed4f0044f7acf35c9c2ec5d2f
Bruno Rivera Juárez <a01571663@tec.mx>
2026-05-11 11:33:27 -0600

    Deploy website - based on 0f6c355acaeedff1a3c56bb6a043fa4887fe4fa1

---
+++ assets/js/639ca82b.9af52904.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([["258"],{4482(e,s,a){a.r(s),a.d(s,{default:()=>r});var c=a(4848);a(6540);var i=a(6525);function r(){return(0,c.jsxs)(i.A,{children:[(0,c.jsx)("h1",{children:"My React page"}),(0,c.jsx)("p",{children:"This is a React page"})]})}}}]);
